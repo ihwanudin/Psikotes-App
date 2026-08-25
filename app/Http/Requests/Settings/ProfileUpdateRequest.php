318cb365bdd8634bafc444e2528b8f88d66c096a
IhwanIU <ihwanoedin@gmail.com>
2026-08-25 14:58:50 +0700

    fix: narrow settings web principals

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Requests\Settings;
 
 use App\Concerns\ProfileValidationRules;
+use App\Http\Requests\Concerns\ResolvesWebUser;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProfileUpdateRequest extends FormRequest
 {
-    use ProfileValidationRules;
+    use ProfileValidationRules, ResolvesWebUser;
 
     /**
      * Get the validation rules that apply to the request.
--- app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
+++ app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\Settings;
 
+use App\Http\Requests\Concerns\ResolvesWebUser;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Laravel\Fortify\InteractsWithTwoFactorState;
 
 class TwoFactorAuthenticationRequest extends FormRequest
 {
-    use InteractsWithTwoFactorState;
+    use InteractsWithTwoFactorState, ResolvesWebUser;
 
     /**
      * Get the validation rules that apply to the request.
